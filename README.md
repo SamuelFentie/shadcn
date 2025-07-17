24737e59496ca1c1af79f516614c786e072e7a7b
Meskir Samuel <samuelfentie55@gmail.com>
2025-07-17 09:08:55 +0300

    +Start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Xero
+Start
---
+++ shdashboard/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
