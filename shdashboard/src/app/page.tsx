13e91e4cc74872bb8d63c13e92172a33e634b06c
Meskir Samuel <samuelfentie55@gmail.com>
2025-07-18 14:50:36 +0300

    42

--- shdashboard/src/app/page.tsx
+++ shdashboard/src/app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex justify-center items-center">
-      <Button variant="destructive" className="text-custom-color">Click me</Button>
     </div>
   );
 }
---
+++ shdashboard/src/components/AppSidebar.tsx
@@ -0,0 +1,108 @@
+import {
+  Home,
+  Inbox,
+  Calendar,
+  Search,
+  Settings,
+  User2,
+  ChevronUp,
+  Plus,
+  Projector,
+  ChevronDown,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarSeparator,
+} from "./ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                        <Link href="/">
+                        <Image src="/next.svg" alt="logo" width={20} height={20}/>
+                        <span>DeV TOolS</span>
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupLabel>Applications</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        {items.map(item=>(
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton asChild>
+                                    <Link href={item.url}>
+                                        <item.icon/>
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter></SidebarFooter>
+    </Sidebar>
+
+  )
+}
+
+export default AppSidebar
